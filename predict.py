039cd4ff70b048a50a1e0d4930af591cd422ea1a
yonggyo1125 <yonggyo00@kakao.com>
2025-07-03 10:49:15 +0900

    .

--- predict.py
+++ predict.py
@@ -1,13 +1,27 @@
 import os
 import sys
+import json
 import keras
 import numpy as np
 from PIL import Image
 
+if len(sys.argv) < 2:
+    sys.exit(1)
+
 # 기준 경로
 base_path = os.path.dirname(os.path.realpath(__file__))
 
+# 이미지 경로 
+image_path = sys.argv[1]
+
 # model 불러오기
-#model = keras.models.load_model(f"${base_path}/best-model.keras")
-model = keras.models.load_model("C:/models/best-model.keras")
-model.summary()
+model = keras.models.load_model(f"{base_path}/best-model.keras")
+
+# 이미지 전처리 - 224 X 224, 이미지 반전
+image = np.invert(np.array(Image.open(image_path).resize((224, 224))))
+
+# 분류목록 조회 
+categories = np.load(base_path + '/category.npy')
+
+# 추론 하기
+ 
